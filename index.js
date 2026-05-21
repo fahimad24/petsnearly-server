1445d9195dd6c9d431309ad24e2229519c07284d
fahimad24 <mdtaraquerahmanfahim@gmail.com>
2026-05-21 21:18:15 +0600

    awit client.connect diseble for deploying

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const verifyToken = async (req, res, next) => {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
         const db = client.db("petsdata");
         const allPetsCollection = db.collection("allpets");
         const adoptionRequestsCollection = db.collection("adoptionRequests");
@@ -217,7 +217,7 @@ async function run() {
         });
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when it is finished with the server
